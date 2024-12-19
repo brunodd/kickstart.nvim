779a646c731c4ffd37c085b1ca19425e525f34bd
Bruno De Deken <bruno.dedeken@nike.com>
2024-12-19 11:16:43 +0100

    Add pyright, ruff, pylint and black LSPs

--- init.lua
+++ init.lua
@@ -634,7 +634,10 @@ require('lazy').setup({
       local servers = {
         -- clangd = {},
         -- gopls = {},
-        -- pyright = {},
+        pyright = {},
+        ruff = {},
+        pylint = {},
+        black = {},
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
